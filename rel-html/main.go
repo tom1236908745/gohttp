17c2ee6e5a8a2ab561f550894769cea832ce1c4b
tom1236908745 <tom1233908745@gmail.com>
2021-09-17 21:48:24 +0900

    モジュールインポートのため

--- rel-html/main.go
+++ rel-html/main.go
@@ -1,24 +1,12 @@
 package main
 
 import (
+	"github.com/tom1236908745/gohttp/twitter"
 	"html/template"
-	"io"
 	"net/http"
-
 	"github.com/labstack/echo"
+	"io"
 )
-
-type Template struct {
-	templates *template.Template
-}
-
-func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
-func Hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "World")
-}
 
 func main() {
 	e := echo.New()
@@ -26,7 +14,43 @@ func main() {
 		templates: template.Must(template.ParseGlob("public/views/*.html")),
 	}
 	e.Renderer = t
-	e.GET("/hello", Hello)
+	e.GET("/tweet", tweet)
 
 	e.Logger.Fatal(e.Start(":8000"))
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func tweet(c echo.Context) error {
+	value := c.QueryParam("value")
+	api := twitter.ConnectTwitterApi()
+	//検索
+	searchResult, _ := api.GetSearch(`"` + value + `"`, nil)
+	tweet := new(TweetTempete)
+	for _, data := range searchResult.Statuses {
+		tweet.Text = data.FullText
+		tweet.User = data.User.Name
+		tweet.Id = data.User.IdStr
+		tweet.ScreenName = data.User.ScreenName
+		tweet.Date = data.CreatedAt
+		tweet.TweetId = data.IdStr
+		break
+	}
+	return c.Render(http.StatusOK, "tweet.html", tweet)
+}
+
+type Template struct {
+	templates *template.Template
+}
+
+// TweetTempete はツイートの情報
+type TweetTempete struct {
+	User string `json:"user"`
+	Text string `json:"text"`
+	ScreenName string `json:"screenName"`
+	Id string `json:"id"`
+	Date string `json:"date"`
+	TweetId string `json:"tweetId"`
 }
