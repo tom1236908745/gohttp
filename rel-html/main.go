387ecc07f53841f9d3d417979f1d5a769d66176d
tom1236908745 <tom1233908745@gmail.com>
2021-09-17 21:20:50 +0900

    twitter apiとの連携

---
+++ rel-html/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"net/http"
+
+	"github.com/labstack/echo"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "World")
+}
+
+func main() {
+	e := echo.New()
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = t
+	e.GET("/hello", Hello)
+
+	e.Logger.Fatal(e.Start(":8000"))
+}
